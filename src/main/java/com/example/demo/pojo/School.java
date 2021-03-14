e3a888cf78b5add89e7900bb9aea91602a808a40
Drgil666 <2574105038@qq.com>
2021-03-14 11:56:33 +0800

    Feats:修改XML的文件名，新增Academy,Major,School类

---
+++ src/main/java/com/example/demo/pojo/School.java
@@ -0,0 +1,19 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+/**
+ * @author Gilbert
+ * @date 2021/3/14 11:31
+ */
+@Data
+public class School {
+    /**
+     * 学校id
+     */
+    private Integer id;
+    /**
+     * 学校名
+     */
+    private String nick;
+}
