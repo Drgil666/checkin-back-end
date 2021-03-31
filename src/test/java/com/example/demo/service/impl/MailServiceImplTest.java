e95f27430df448206985287fa0faacec24bc8afc
Drgil666 <2574105038@qq.com>
2021-03-31 10:41:11 +0800

    Feats:新增邮件测试类

---
+++ src/test/java/com/example/demo/service/impl/MailServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.MailService;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.Resource;
+
+/**
+ * @author DrGilbert
+ * @date 2021/3/31 10:40
+ */
+public class MailServiceImplTest {
+    @Resource
+    private MailService mailService;
+
+    @Test
+    void sendSimpleMail() {
+    }
+
+    @Test
+    void sendHtmlMail() {
+    }
+
+    @Test
+    void sendAttachmentsMail() {
+    }
+}
