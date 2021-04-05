faba0ecf6f961fad017e178242c4e84347b812b5
Drgil666 <2574105038@qq.com>
2021-04-05 15:59:21 +0800

    Feats:新增KaptchaController

---
+++ src/main/java/com/example/demo/controller/KaptchaController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Gilbert
+ * @date 2021/4/5 15:24
+ */
+@Controller
+@RequestMapping("/api/kaptcha")
+public class KaptchaController {
+
+}
