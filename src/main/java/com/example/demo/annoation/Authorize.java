ddbcf9f1adf95bbcf46791d4cfee8739dc493c78
Drgil666 <2574105038@qq.com>
2021-04-16 16:20:08 +0800

    Feats:视图添加自定义注解Authorize(待施工)

---
+++ src/main/java/com/example/demo/annoation/Authorize.java
@@ -0,0 +1,15 @@
+package com.example.demo.annoation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Gilbert
+ * @date 2021/4/16 16:07
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface Authorize {
+    String value();
+}
